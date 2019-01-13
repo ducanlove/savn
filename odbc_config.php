038e9ea8059f378b9b10fcc1c0e167fd0ec5a424
ducanlove <46642293+ducanlove@users.noreply.github.com>
2019-01-13 21:32:31 +0700

    Add files via upload

---
+++ odbc_config.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+include ('includes/security/antisql.php');
+
+$dbname = "LOW_DB";
+$dbuser = "sa";
+$dbpass = "Sudden123";
+
+$conn = odbc_connect("Driver={SQL Server};Server=INSTANCE-1,1433;Charset=utf8;Database=LOW_DB","$dbuser","$dbpass");
+
+/* Kayit Ayarlarý */
+$kayit = "acik"; // Üyelik alýmý
+$sifredegisimi = "kapat"; //þifre hatýrlatma deðiþtirme
+
+/* Tarih Ayarlarý (Düzenlemeyiniz!!!) */
+
+date_default_timezone_set('Europe/Istanbul');
+$todayh = getdate();
+$d = $todayh["mday"];
+$m = $todayh["mon"];
+$y = $todayh["year"];	
+$date = $d."/".$m."/".$y;
+
+
+?>
